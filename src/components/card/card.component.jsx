83ecf25f3b89c6be1cc6368746a5f2bd9aede119
Bekzat Omrzakov <bomrzak@gap.com>
2020-05-10 22:00:57 -0700

    card and card-list components

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.style.css';
+
+export const Card = (props) =>  (
+    <div className="card-container">
+        <img 
+            src={`https://robohash.org/${props.monster.id}?set=set2?size=200x200`} 
+            alt="monster"/>
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
+    </div>
+)
