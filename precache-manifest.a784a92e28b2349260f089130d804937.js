20b2a66b011e52ad4ef5ea50130eb6bf0565f4e4
Bekzat Omrzakov <bomrzak@gap.com>
2020-05-12 00:19:26 -0700

    Updates

---
+++ precache-manifest.a784a92e28b2349260f089130d804937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02e0b7e4f55935ec5c77d9690c7ae62",
+    "url": "/search-monsters/index.html"
+  },
+  {
+    "revision": "9f66752ffc9f73764223",
+    "url": "/search-monsters/static/css/main.c66cdea7.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/search-monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f66752ffc9f73764223",
+    "url": "/search-monsters/static/js/main.bba0a7f9.chunk.js"
+  },
+  {
+    "revision": "175f0207ed456b8b8830",
+    "url": "/search-monsters/static/js/runtime-main.0046e25c.js"
+  }
+]);
