0ae53e7f6d33ebc44496d95ecd82f5d3740cf84f
Bekzat Omrzakov <bomrzak@gap.com>
2020-05-12 00:48:45 -0700

    arr fetch error handling

--- src/App.js
+++ src/App.js
@@ -14,9 +14,13 @@ class App extends Component {
   }
 
   componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => this.setState({monsters: users}),
+      (error) => {
+        console.log('Request failed', error)
+      }
+    )
   };
 
   handleChange = (e) => {
